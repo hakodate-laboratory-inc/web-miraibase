4d9ae855b3dfde47d824b7d6364ad5c25be2a2fa
Shota Kawashima <bysk.333@gmail.com>
2018-04-05 17:48:11 +0900

    moment.jsを使用した時間表示

--- src/script/loadConnpassEvent.js
+++ src/script/loadConnpassEvent.js
@@ -1,5 +1,7 @@
 import Vue from 'vue/dist/vue.js'
 import axios from 'axios'
+import 'moment/src/locale/ja'
+import moment from 'moment'
 
 const eventVue = new Vue({
   el: '#event',
@@ -9,19 +11,22 @@ const eventVue = new Vue({
         title: '',
         link: '',
         desc: '',
-        imageSrc: ''
+        imageSrc: '',
+        startDate: ''
       },
       {
         title: '',
         link: '',
         desc: '',
-        imageSrc: ''
+        imageSrc: '',
+        startDate: ''
       },
       {
         title: '',
         link: '',
         desc: '',
-        imageSrc: ''
+        imageSrc: '',
+        startDate: ''
       }
     ],
     thumbnailStyle: 'display: none',
@@ -43,6 +48,8 @@ const eventVue = new Vue({
         res.data.events.forEach((resEvent, i) => {
           eventVue.events[i].title = resEvent.title
           eventVue.events[i].link = resEvent.event_url
+          // eventVue.events[i].startDate = moment(resEvent.started_at).format('YYYY年M月D日ddd H:mm')
+          eventVue.events[i].startDate = moment(resEvent.started_at).format('YYYY年M月D日(ddd) H:mm')
           eventVue.events[i].desc = /<p>(.+?)<\/p>/g.exec(resEvent.description)[1]
           axios.get('http://hakolab.co.jp/api/avoidACAO.cgi?url=' + resEvent.event_url)
             .then(function (resres) {
