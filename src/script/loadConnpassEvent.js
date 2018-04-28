1de4692458aad899c1d22ef95a8e9838f4bf051b
Shota Kawashima <bysk.333@gmail.com>
2018-04-28 15:25:35 +0900

    リファクタリングとエラーのconsolelogまで

--- src/script/loadConnpassEvent.js
+++ src/script/loadConnpassEvent.js
@@ -4,30 +4,7 @@ import axios from 'axios'
 const eventVue = new Vue({
   el: '#event',
   data: {
-    events: [
-      {
-        title: '',
-        link: '',
-        desc: '',
-        imageSrc: '',
-        thumbnailStyle: 'display: none',
-        loadingStyle: ''
-      },
-      {
-        title: '',
-        link: '',
-        desc: '',
-        imageSrc: '',
-        thumbnailStyle: 'display: none',
-        loadingStyle: ''
-      },
-      {
-        title: '',
-        link: '',
-        desc: '',
-        imageSrc: ''
-      }
-    ],
+    events: [],
     thumbnailStyle: 'display: none',
     loadingStyle: '',
     loadedThumbnails: 0
@@ -42,14 +19,20 @@ const eventVue = new Vue({
     }
   },
   mounted () {
-    axios.get('http://hakolab.co.jp/api/loadConnpass.cgi')
-      .then(function (res) {
+    axios.get('http://halab.co.jp/api/loadConnpass.cgi')
+      .then((res) => {
         res.data.events.sort(function (a, b) {
           if (a.started_at < b.started_at) return -1
           if (a.started_at > b.started_at) return 1
           return 0
         })
         res.data.events.forEach((resEvent, i) => {
+          eventVue.events.push({
+            title: '',
+            link: '',
+            desc: '',
+            imageSrc: ''
+          })
           eventVue.events[i].title = resEvent.title
           eventVue.events[i].link = resEvent.event_url
           eventVue.events[i].desc = /<p>(.+?)<\/p>/g.exec(resEvent.description)[1]
@@ -58,6 +41,11 @@ const eventVue = new Vue({
               eventVue.events[i].imageSrc = /background-image:url\((\S*)\)/.exec(resres.data)[1]
             })
         })
+      })
+      .catch(() => {
+        console.log('err')
+        this.thumbnailStyle = 'display: none'
+        this.loadingStyle = 'display: none'
       })
   }
 })
