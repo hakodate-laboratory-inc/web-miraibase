c1c00ee371cdc760abf1829144ebbbd594dfc1c5
Shota Kawashima <bysk.333@gmail.com>
2018-05-09 17:04:16 +0900

    調整終了

--- src/script/loadConnpassEvent.js
+++ src/script/loadConnpassEvent.js
@@ -43,7 +43,7 @@ const eventVue = new Vue({
     }
   },
   mounted () {
-    axios.get('http://hakolab.co.jp/api/loadConnpass.cgi')
+    axios.get('http://hakolab.co.jp/api/loadConnpassDummy.cgi')
       .then(function (res) {
         res.data.events.sort(function (a, b) {
           if (a.started_at < b.started_at) return -1
@@ -54,7 +54,6 @@ const eventVue = new Vue({
           eventVue.events[i].title = resEvent.title
           eventVue.events[i].link = resEvent.event_url
           eventVue.events[i].startDate = moment(resEvent.started_at).format('YYYY年M月D日(ddd) H:mm')
-          // eventVue.events[i].startDate = moment(resEvent.started_at).format('llll') // 51行目と同義
           eventVue.events[i].desc = /<p>(.+?)<\/p>/g.exec(resEvent.description)[1]
           axios.get('http://hakolab.co.jp/api/avoidACAO.cgi?url=' + resEvent.event_url)
             .then(function (resres) {
