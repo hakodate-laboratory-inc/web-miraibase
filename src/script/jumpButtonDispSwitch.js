6ca2ddc9222bbc22877a0e3cf9643ced5ae2bf55
ryu-g <em.ryu.g@gmail.com>
2019-02-28 18:20:18 +0900

    jumpButton SP版で非表示にした

--- src/script/jumpButtonDispSwitch.js
+++ src/script/jumpButtonDispSwitch.js
@@ -1,12 +1,11 @@
 el = document.getElementById('jump')
 
 window.addEventListener('scroll', DisplaySwitch)
-
 function DisplaySwitch(){
+  let mediaQuery = matchMedia('(max-width: 798px');
   let y = window.pageYOffset
-  console.log(y);
 
-  if(y > 680){
+  if(y > 680 && !mediaQuery.matches){
     el.style.opacity = "1"
     el.style.bottom = "-100px"
   }
@@ -14,5 +13,4 @@ function DisplaySwitch(){
     el.style.opacity = "0"
     el.style.bottom = "-200px"
   }
-  console.log("hello")
 }
