9f8e7889532f242fa81a53b89794f8d731909f42
ryu-g <em.ryu.g@gmail.com>
2019-02-28 18:33:25 +0900

    きれいにした

--- src/script/jumpButtonDispSwitch.js
+++ src/script/jumpButtonDispSwitch.js
@@ -1,15 +1,14 @@
-el = document.getElementById('jump')
+const el = document.getElementById('jump')
 
 window.addEventListener('scroll', DisplaySwitch)
-function DisplaySwitch(){
-  let mediaQuery = matchMedia('(max-width: 798px');
-  let y = window.pageYOffset
+function DisplaySwitch () { 
+  const mediaQuery = matchMedia('(max-width: 798px')
 
-  if(y > 680 && !mediaQuery.matches){
-    el.style.opacity = "1"
-    el.style.bottom = "-100px"
-  } else{
-    el.style.opacity = "0"
-    el.style.bottom = "-200px"
+  if (window.pageYOffset > 680 && !mediaQuery.matches) {
+    el.style.opacity = '1'
+    el.style.bottom = '-100px'
+  } else {
+    el.style.opacity = '0'
+    el.style.bottom = '-200px'
   }
 }
