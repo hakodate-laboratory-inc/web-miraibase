6c4115d6c6e434f14534298829a2c2b73d1c4010
sKawashima <bysk.333@gmail.com>
2019-09-13 22:59:52 +0900

    Cant resolve err:.rendar is not a function

---
+++ src/script/viewCalendar.js
@@ -0,0 +1,14 @@
+import { Calendar } from '@fullcalendar/core'
+import timeGridPlugin from '@fullcalendar/timegrid'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const calendarElement = document.getElementById('gcal')
+
+  let calendar = new Calendar(calendarElement, {
+    plugins: [ timeGridPlugin ],
+    defaultView: 'timeGridWeek'
+  })
+
+  console.log(calendar)
+  calendar.rendar()
+})
