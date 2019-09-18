9d7a7fd0aaf805d9b1b0c2af5ded8ebc401867e7
sKawashima <bysk.333@gmail.com>
2019-09-18 23:02:48 +0900

    :sparkles: Get google calendar data

--- src/script/showCalendar.js
+++ src/script/showCalendar.js
@@ -1,5 +1,6 @@
 import { Calendar } from '@fullcalendar/core'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
 import '@fullcalendar/core/main.css'
 import '@fullcalendar/timegrid/main.css'
 
@@ -7,9 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const calendarElement = document.getElementById('gcal')
 
   const calendar = new Calendar(calendarElement, {
-    plugins: [timeGridPlugin],
+    plugins: [timeGridPlugin, googleCalendarPlugin],
     defaultView: 'timeGridWeek',
-    height: 500
+    height: 500,
+    googleCalendarApiKey: 'AIzaSyBB7eXTLFmD4iCU7roC--Eoem5h8ZYQuZg',
+    events: {
+      googleCalendarId: 'pmhd53aqkpdoqtd1qtj5kluo64@group.calendar.google.com'
+    }
   })
 
   calendar.render()
